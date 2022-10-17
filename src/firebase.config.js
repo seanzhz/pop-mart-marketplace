2336edd30c96dcfec18cbe524d160603e8456438
seanzhz <zhz11433012@gmail.com>
2022-10-17 12:47:48 +1100

    Initial

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFireStore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCortdZnrQynkuUL6zFEfV5vgyJdcvLoXs",
+    authDomain: "pop-item-app.firebaseapp.com",
+    projectId: "pop-item-app",
+    storageBucket: "pop-item-app.appspot.com",
+    messagingSenderId: "132470869612",
+    appId: "1:132470869612:web:c57aba79cbc7774a185249"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db =getFireStore()
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Signup(props) {
+    return (
+        <div>
+            <h1>Sign up</h1>
+        </div>
+    );
+}
+
+export default Signup;
